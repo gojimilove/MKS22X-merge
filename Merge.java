71b61e63e61c62790e12483c6ebc33ab254feea7
Jessica Kim <jkim00@stuy.edu>
2019-03-15 11:48:08 -0400

    started mergesort and private helper method

--- Merge.java
+++ Merge.java
@@ -1,5 +1,14 @@
 public class Merge {
   public static void main(String[]args) {
-    
+
+  }
+  /*sort the array from least to greatest value. This is a wrapper function*/
+  public static void mergesort(int[]data){
+    mergesort(data, 0, data.length-1);
+  }
+
+  // helper method
+  private static void mergesort(int[]data, int[]temp, int lo, int hi){
+
   }
 }
