331c5b36aea0bca6619c7f5b958512e874e01f43
Jessica Kim <jkim00@stuy.edu>
2019-03-18 11:48:34 -0400

    more work on helper method

--- Merge.java
+++ Merge.java
@@ -1,14 +1,27 @@
+import java.util.*;
+
 public class Merge {
   public static void main(String[]args) {
 
   }
   /*sort the array from least to greatest value. This is a wrapper function*/
   public static void mergesort(int[]data){
-    mergesort(data, 0, data.length-1);
+    int[]temp = new int[data.length];
+    mergesort(data, temp, 0, data.length);
   }
 
   // helper method
   private static void mergesort(int[]data, int[]temp, int lo, int hi){
+    if (lo >= hi) {
+      return;
+    }
+    int[] loData = Arrays.copyOfRange(data, lo, hi/2);
+    int[] hiData = Arrays.copyOfRange(data, hi/2, hi);
+    //mergesort left side
+
+    //mergesort right side
+
+    //merge
 
   }
 }
