6dc50dad7fd7df62d839101eec765876a3c6e09b
Jessica Kim <jkim00@stuy.edu>
2019-03-19 17:43:38 -0400

    removed int[]temp didnt use it

--- Merge.java
+++ Merge.java
@@ -3,7 +3,6 @@ import java.util.*;
 public class Merge {
   /*sort the array from least to greatest value. This is a wrapper function*/
   public static void mergesort(int[]data){
-    int[]temp = new int[data.length];
     mergesort(data, 0, data.length-1);
   }
 
